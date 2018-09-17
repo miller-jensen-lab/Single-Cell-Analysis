a38e87c8efbbef96eecda56dcc28535c7aac18a1
Andres Munoz <andres@Andress-MacBook-Pro.local>
2018-09-17 11:32:56 -0400

    deleted merged files manually

--- v3.0/aux/extract_from.m
+++
@@ -1,5 +0,0 @@
-%Read fcs files from cyt and get %s
-[fcsdata fcsother] = fca_readfcs
-binarydat=fcsdata(:,4:18)>asinh(1/0.8);
-signal_names=vertcat({fcsother.par(4:18).name2});
-per_on=round((sum(binarydat)/length(binarydat))*100,2);
